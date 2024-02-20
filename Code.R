566f048fe3e9d26e4f380d10e5fac361111e2a1b
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-02-20 14:40:45 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,85 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(soiltestcorr)
+
+corr_df <- soiltestcorr::data_test
+
+quadratic_plateau(corr_df, STV, RY, tidy = TRUE)
+
+
+quadratic_plateau(corr_df, STV, RY, tidy = FALSE)
+
+
+fit_vectors_tidy <- quadratic_plateau(stv = corr_df$STV, ry = corr_df$RY)
+
+fit_vectors_list <- quadratic_plateau(stv = corr_df$STV, ry = corr_df$RY, tidy = FALSE)
+
+data_1 <- data.frame("RY"  = c(65,80,85,88,90,94,93,96,97,95,98,100,99,99,100),
+                     "STV" = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
+
+
+data_2 <- soiltestcorr::data_test
+
+data_3 <- soiltestcorr::freitas1966 %>% 
+  rename(STV = STK)
+
+data.all <- bind_rows(data_1, data_2, data_3, .id = "id")
+
+
+data.all %>%
+  nest(data = c("STV", "RY")) %>% 
+  mutate(model = map(data, ~ quadratic_plateau(stv = .$STV, ry = .$RY))) %>%
+  unnest(model)
+
+data.all %>% 
+  group_by(id) %>% 
+  group_modify(~ quadratic_plateau(data = ., STV, RY))
+
+boot_qp <- boot_quadratic_plateau(corr_df, STV, RY, n = 500) # only 500 for sake of speed
+
+boot_qp %>% head(n = 5)
+
+
+quantile(boot_qp$CSTV, probs = c(0.025, 0.5, 0.975))
+
+
+boot_qp %>% 
+  ggplot2::ggplot(aes(x = CSTV))+
+  geom_histogram(color = "grey25", fill = "#9de0bf", bins = 10)
+
+
+data_3 <- soiltestcorr::freitas1966
+
+plot_qp <- quadratic_plateau(data = data_3, STK, RY, plot = TRUE)
+
+plot_qp
+
+
+plot_qp +
+  # Main title
+  ggtitle("My own plot title")+
+  # Axis titles
+  labs(x = "Soil Test K (ppm)",
+       y = "Cotton RY(%)") +
+  # Axis scales
+  scale_x_continuous(limits = c(20,220),
+                     breaks = seq(0,220, by = 10))+
+  # Axis limits
+  scale_y_continuous(limits = c(30, 110),
+                     breaks = seq(30, 110, by = 10))
+
+
+# Residuals plot
+quadratic_plateau(data = data_3, STK, RY, resid = TRUE)
+
+
+
+
+
+
+
+
+
+
